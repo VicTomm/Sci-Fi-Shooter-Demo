faf3589cd60dba36f8e8b2d8f7d59e8ba19b9cb2
Victor Tommasi <vtomm6575@gmail.com>
2018-08-09 15:34:52 -0500

    uploading Shooter demo

---
+++ Assets/Game/Scripts/Player.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 3.5f;
+    private float _gravity = 9.18f;
+    [SerializeField]
+    private GameObject _muzzleFlash;
+    [SerializeField]
+    private GameObject _hitMarkerPrefab;
+    [SerializeField]
+    private AudioSource _weaponAudio;
+    [SerializeField]
+    private int currentAmmo;
+    private int maxAmmo = 50;
+
+    private bool _isReloading = false;
+
+    private UIManager _uiManager;
+
+    //variable for has coin
+    public bool hasCoin = false;
+
+    [SerializeField]
+    private GameObject _weapon;
+
+    // Use this for initialization
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        //hide mouse cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        _muzzleFlash.SetActive(false);
+
+        currentAmmo = maxAmmo;
+
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && currentAmmo > 0)
+        {
+            Shoot();
+        }
+        else
+        {
+            _muzzleFlash.SetActive(false);
+            _weaponAudio.Stop();
+        }
+
+        if(Input.GetKeyDown(KeyCode.R) && !_isReloading)
+        {
+            _isReloading = true;
+            StartCoroutine(ReloadAfterTime(1.5f));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        CalculateMovement();
+    }
+
+    void Shoot()
+    {
+        _muzzleFlash.SetActive(true);
+        currentAmmo--;
+        _uiManager.UpdateAmmo(currentAmmo);
+        if (!_weaponAudio.isPlaying)
+        {
+            _weaponAudio.Play();
+        }
+        Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(rayOrigin, out hitInfo))
+        {
+            GameObject hitMarker = Instantiate(_hitMarkerPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal)) as GameObject;
+            Destroy(hitMarker, 1f);
+
+            //check if we hit the crate
+            //Destroy Crate
+
+            Destructable crate = hitInfo.transform.GetComponent<Destructable>();
+            if (crate != null)
+            {
+                crate.DestroyCrate();
+            }
+        }
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        Vector3 velocity = direction * _speed;
+
+        velocity = transform.transform.TransformDirection(velocity);
+        velocity.y -= _gravity;
+        _controller.Move(velocity * Time.deltaTime);
+    }
+
+    IEnumerator ReloadAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        currentAmmo = maxAmmo;
+        _uiManager.UpdateAmmo(currentAmmo);
+        _isReloading = false;
+    }
+
+    public void EnableWeapons()
+    {
+        _weapon.SetActive(true);
+    }
+}
